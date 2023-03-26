f90eb2bbdca5d301979484c0d690174233989c14
Ciprian <botnari_ciprian@mail.ru>
2023-03-26 13:42:01 +0300

    Add lexer for simple input

--- src/Main.java
+++ src/Main.java
@@ -1,91 +1,17 @@
-import automaton.FiniteAutomaton;
-import automaton.Transition;
-import grammar.Grammar;
-import grammar.Production;
+import lexer.Lexer;
+import lexer.Token;
 
-//    Variant 6: First laboratory work
-//        Vn={S, I, J, K},
-//        Vt={a, b, c, e, n, f, m},
-//        P = {
-//        S → cI
-//        I → bJ
-//        I → fI
-//        J → nJ
-//        J → cS
-//        I → eK
-//        K → nK
-//        I → e
-//        K → m
-//    }
-
-//      Variant 6: Second laboratory work
-//        Q = {q0,q1,q2,q3,q4},
-//        ∑ = {a,b},
-//        F = {q4},
-//        δ(q0,a) = q1,
-//        δ(q1,b) = q1,
-//        δ(q1,b) = q2,
-//        δ(q2,b) = q3,
-//        δ(q3,a) = q1,
-//        δ(q2,a) = q4.
+import java.util.List;
 
 public class Main
 {
     public static void main(String[] args) {
-        Grammar grammar = new Grammar(
-                new String[]{"S", "I", "J", "K"},
-                new String[]{"a", "b", "c", "e", "n", "f", "m"},
-                new Production[]{
-                        new Production("S", "cI"),
-                        new Production("I", "bJ"),
-                        new Production("I", "fI"),
-                        new Production("J", "nJ"),
-                        new Production("J", "cS"),
-                        new Production("I", "eK"),
-                        new Production("K", "nK"),
-                        new Production("I", "e"),
-                        new Production("K", "m"),
-                },
-                "S"
-        );
+        String input = "5+(6-2)*3/4";
+        Lexer lexer = new Lexer(input);
 
-        FiniteAutomaton FA = new FiniteAutomaton(
-                new String[]{"q0", "q1", "q2", "q3", "q4"},
-                new String[]{"a", "b"},
-                new Transition[]{
-                        new Transition("q0", "a", "q1"),
-                        new Transition("q1", "b", "q1"),
-                        new Transition("q1", "b", "q2"),
-                        new Transition("q2", "b", "q3"),
-                        new Transition("q3", "a", "q1"),
-                        new Transition("q2", "a", "q4"),
-                },
-                "q0",
-                new String[]{"q4"}
-        );
-
-        System.out.println("\n 1. Classify the grammar based on Chomsky hierarchy: ");
-        System.out.println(grammar);
-        System.out.println("===== Current grammar is of: " + grammar.classifyGrammar() + " =====");
-
-        System.out.println("\n 2. Convert the finite automaton to regular grammar: ");
-        Grammar FAtoGrammar = FA.convertToRegularGrammar();
-        System.out.println(FA);
-        System.out.println(FAtoGrammar);
-
-        System.out.println("\n 2.1. Generate random words from converted grammar: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(FAtoGrammar.generateWord());
+        List<Token> tokens = lexer.tokenize();
+        for (Token token : tokens) {
+            System.out.println(token);
         }
-
-        System.out.println("\n 3. Check if Finite Automaton is deterministic: ");
-        System.out.println("===== FA is deterministic: " + FA.isDeterministic() + " =====");
-
-        System.out.println("\n 4. NFA to DFA: ");
-        System.out.println("===== DFA: " + FA.convertToDFA() + " =====");
-
-        System.out.println("\n 5. Display Finite Automaton graph: ");
-        System.out.println(FA);
-        FA.showGraph();
     }
 }
--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -1,29 +1,168 @@
 package lexer;
 
 import automaton.FiniteAutomaton;
+import automaton.Transition;
 import grammar.Grammar;
+import lexer.Token;
+import lexer.TokenType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lexer {
   private final FiniteAutomaton automaton;
   private final Grammar grammar;
-  private String input;
-  private int index;
+  private final String input;
+  private int position;
 
   public Lexer(FiniteAutomaton automaton, Grammar grammar, String input) {
     this.automaton = automaton;
     this.grammar = grammar;
     this.input = input;
-    this.index = 0;
+    this.position = 0;
   }
 
-  public Token nextToken() {
-    // Use the finite automaton to determine the next token
-    String tokenText = automaton.nextToken(input, index);
-    TokenType tokenType = grammar.getTokenType(tokenText);
-
-    // Create and return the token
-    Token token = new Token(tokenType, tokenText);
-    index += tokenText.length();
-    return token;
+  public Lexer(String input) {
+    this.automaton = null;
+    this.grammar = null;
+    this.input = input;
+    this.position = 0;
   }
+
+//  public List<Token> tokenize() {
+//    List<Token> tokens = new ArrayList<>();
+//
+//    while (position < input.length()) {
+//      int maxMatchLength = 0;
+//      Transition matchingTransition = null;
+//
+//      for (Transition transition : automaton.getTransitions()) {
+//        if (transition.getSource().equals(automaton.getCurrentState())
+//                && transition.matches(input.charAt(position))) {
+//          if (transition.getLabel().length() > maxMatchLength) {
+//            maxMatchLength = transition.getLabel().length();
+//            matchingTransition = transition;
+//          }
+//        }
+//      }
+//
+//      if (matchingTransition != null) {
+//        automaton.setCurrentState(matchingTransition.getTarget());
+//        position += maxMatchLength;
+//      } else {
+//        throw new IllegalArgumentException("Invalid input at position " + position);
+//      }
+//
+//      if (automaton.isFinalState()) {
+//        TokenType tokenType = getTokenType(automaton.getCurrentState());
+//        String lexeme = input.substring(position - automaton.getCurrentState().length(), position);
+//        tokens.add(new Token(tokenType, lexeme));
+//        automaton.reset();
+//      }
+//    }
+//
+//    tokens.add(new Token(TokenType.END_OF_INPUT, ""));
+//    return tokens;
+//  }
+//
+//  private TokenType getTokenType(String state) {
+//    for (int i = 0; i < grammar.getNonTerminalVariables().length; i++) {
+//      if (state.equals(grammar.getNonTerminalVariables()[i])) {
+//        return TokenType.values()[i];
+//      }
+//    }
+//
+//    for (int i = 0; i < grammar.getTerminalVariables().length; i++) {
+//      if (state.equals(grammar.getTerminalVariables()[i])) {
+//        return TokenType.values()[grammar.getNonTerminalVariables().length + i];
+//      }
+//    }
+//
+//    throw new IllegalStateException("Invalid state: " + state);
+//  }
+
+  public List<Token> tokenize() {
+    List<Token> tokens = new ArrayList<>();
+    int position = 0;
+
+    while (position < input.length()) {
+      int maxMatchLength = 0;
+      TokenType tokenType = null;
+
+      for (TokenType type : TokenType.values()) {
+        String pattern = getTokenPattern(type);
+
+        if (pattern != null) {
+          Pattern regex = Pattern.compile(pattern);
+          Matcher matcher = regex.matcher(input.substring(position));
+
+          if (matcher.lookingAt()) {
+            int matchLength = matcher.end();
+
+            if (matchLength > maxMatchLength) {
+              maxMatchLength = matchLength;
+              tokenType = type;
+            }
+          }
+        }
+      }
+
+      if (maxMatchLength == 0) {
+        throw new IllegalArgumentException("Invalid input at position " + position);
+      }
+
+      String lexeme = input.substring(position, position + maxMatchLength);
+      tokens.add(new Token(tokenType, lexeme));
+      position += maxMatchLength;
+    }
+
+    tokens.add(new Token(TokenType.END_OF_INPUT, ""));
+    return tokens;
+  }
+
+
+  private String getTokenPattern(TokenType type) {
+    return switch (type) {
+      case IF -> "if";
+      case ELSE -> "else";
+      case WHILE -> "while";
+      case FOR -> "for";
+      case RETURN -> "return";
+      case BOOLEAN -> "true|false";
+      case INT -> "\\d+";
+      case CHAR -> "'.'";
+      case STRING -> "\".*?\"";
+      case FLOAT -> "\\d+\\.\\d+";
+      case DOUBLE -> "\\d+\\.\\d+";
+      case VOID -> "void";
+      case PLUS -> "\\+";
+      case MINUS -> "-";
+      case MULTIPLY -> "\\*";
+      case DIVIDE -> "/";
+      case MODULO -> "%";
+      case ASSIGNMENT -> "=";
+      case EQUALS -> "==";
+      case NOT_EQUALS -> "!=";
+      case LESS_THAN -> "<";
+      case LESS_THAN_OR_EQUAL_TO -> "<=";
+      case GREATER_THAN -> ">";
+      case GREATER_THAN_OR_EQUAL_TO -> ">=";
+      case AND -> "&&";
+      case OR -> "\\|\\|";
+      case NOT -> "!";
+      case LEFT_PAREN -> "\\(";
+      case RIGHT_PAREN -> "\\)";
+      case LEFT_BRACE -> "\\{";
+      case RIGHT_BRACE -> "\\}";
+      case SEMICOLON -> ";";
+      case COMMA -> ",";
+      case IDENTIFIER -> "[a-zA-Z_][a-zA-Z_0-9]*";
+      case WHITESPACE -> "\\s+";
+      default -> null;
+    };
+  }
+
+
 }
